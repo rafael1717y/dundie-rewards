14df174d402b671d0fb2586d86ff4db4e0f2a833
Rafael Gomes <rafaelufjf@gmai.com>
2022-05-23 13:29:15 -0300

    Add CI GA

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
     long_description=read("README.md"),
     long_description_content_type="texto/markdown",
     author="Rafael Gomes",
+    python_requires=">=3.8",
     packages=find_packages(), 
     entry_points= {
     "console_scripts": [
