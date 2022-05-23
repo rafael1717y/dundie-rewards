a2ff7a112328ea6cf12a35287a04cfadd4640d60
Rafael Gomes <rafaelufjf@gmai.com>
2022-05-23 07:06:29 -0300

    Set up project initial organization and metadata
    - Add setup.py to turn installable
    - Add __main__ for executable
    - Add __init__ for packages
    
    closes #8

---
+++ dundie/__main__.py
@@ -0,0 +1,3 @@
+# Arquivo principal do programa que permite ao programa ser # executado
+# diretamente. [python -m dundie] 
+print("Executing entry point for dundie...")
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="dundie",
+    version="0.1.0", # Semantic Versioning
+    description="Reward Point System for Dunder Mifflin",
+    author="Rafael Gomes",
+    packages=find_packages(), 
+)
