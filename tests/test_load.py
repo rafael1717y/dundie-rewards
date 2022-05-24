23c4b7ed94b416c6aeee82dc0096f5feb5f36a9e
Rafael Gomes <rafaelufjf@gmai.com>
2022-05-24 12:53:48 -0300

    making an error

--- tests/test_load.py
+++ tests/test_load.py
@@ -5,48 +5,15 @@ from dundie.core import load
 from .constants import PEOPLE_FILE
 
 
-# hooks tradicionais
-def setup_module():
-    print('Roda antes dos testes desse módulo.\n')
-    print()
-
-
-def teardown_module():
-    print()
-    print("Roda apos os testes desse módulo.\n")
-
-
-
-@pytest.fixture(scope="function", autouse=True)
-def create_new_file(tmpdir):
-    file_ = tmpdir.join("new_file.txt")
-    file_.write("isso é sujeira...")
-    yield
-    file_.remove()
-
+@pytest.mark.unit
+@pytest.mark.high
+def test_load_positive_has_2_people(request):
+    """Teste load function"""
+    assert len(load(PEOPLE_FILE)) == 2
 
 
 @pytest.mark.unit
 @pytest.mark.high
-def test_load(request):
+def test_load_positive_has_first_name_starts_with_j(request):
     """Teste load function"""
-    filepath = f"arquivo_indesejado-{uuid.uuid4()}.txt" # apaga o arquivo ao final tmp
-    request.addfinalizer(lambda: os.unlink(filepath)) 
-    
-    with open(filepath, "w") as file_: #teste cria o arquivo
-        file_.write("dados uteis somente paa o teste.")
-    
-    assert len(load(PEOPLE_FILE)) == 2
-    assert load(PEOPLE_FILE)[0][0] == 'J'
-
-
-@pytest.mark.unit
-@pytest.mark.high
-def test_load2():
-    """Teste load function"""
-    
-    with open(f"arquivo_indesejado-{uuid.uuid4()}.txt", "w") as file_:
-        file_.write("dados uteis somente paa o teste.")
-    
-    assert len(load(PEOPLE_FILE)) == 2
     assert load(PEOPLE_FILE)[0][0] == 'J'
